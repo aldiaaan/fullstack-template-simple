991fc98659a89140bbd00a7033af9f5ba9d8aa2c
aldiaaan <aldian.asmr@gmail.com>
2025-05-30 07:23:23 +0700

    feat: login page

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="antialiased">
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,9 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("/trpc/*", "routes/trpc.tsx"),
+  layout("routes/auth/layout.tsx", [
+    route("login", "routes/auth/login.tsx")
+  ]),
 ] satisfies RouteConfig;
