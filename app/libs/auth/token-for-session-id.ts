7a8131edc4511485879c737a48f610e259cf0ab1
aldiaaan <aldian.asmr@gmail.com>
2025-06-21 10:05:06 +0700

    fix: disable loggin token to sessionId func

--- app/libs/auth/token-for-session-id.ts
+++ app/libs/auth/token-for-session-id.ts
@@ -2,8 +2,5 @@ import { sha256 } from "@oslojs/crypto/sha2";
 import { encodeHexLowerCase } from "@oslojs/encoding";
 
 export function tokenForSessionId(token: string) {
-  console.log({
-    id: encodeHexLowerCase(sha256(new TextEncoder().encode(token))),
-  });
   return encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
 }
